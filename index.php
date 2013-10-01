5d3f5a3b1ddc55302adf2a0438ba3551fa7b2b8a
ashtonb <ashtonb110@gmail.com>
2013-10-01 17:45:27 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Senator Online Voting - Build #4</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<p style="text-align:center;"><img src="images/logo.png" alt="Senator Online"/></p>
+<h1>Vote beta</h1>
+<form style="text-align:center;">
+<p><input type="text" name="voterNo" value="Voter Number"/></p>
+
+<p><input type="password" name="voterNo" value="Password"/><br/>
+Forgotten your password?</p>
+
+<p><input type="button" value="Log On" onClick="parent.location='dashboard.php'"/></p>
+</form>
+<p  style="text-align:center;"><a href="enrol.htm">Enrol to Vote</a></p><p  style="text-align:center;"><a href="help.php">Help</a></p>
+</body>
+
+</html> 
